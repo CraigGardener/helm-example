dee3ee2a170a52d7708f4f9af7066101abbfe081
Craig Gardener <craig@craiggardener.uk>
2018-05-24 05:11:55 +0100

    feat(release):

--- release.sh
+++ release.sh
@@ -2,4 +2,4 @@
 
 git checkout master
 git merge develop
-sed -i '' -e "s/^version.*/version: ${nextRelease.version}/" Chart.yaml
+sed -i "" -e "s/^version.*/version: ${nextRelease.version}/" Chart.yaml
